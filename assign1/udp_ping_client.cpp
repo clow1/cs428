9e99dbd9143e84c67614eb377044fc515658b5b5
crystallow97 <clow1@binghamton.edu>
2020-02-28 15:17:49 -0500

    documented code, need to add RTT stuff

--- assign1/udp_ping_client.cpp
+++ assign1/udp_ping_client.cpp
@@ -16,12 +16,15 @@
 
 int
 main() {
-  int sockfd, n;
-  //struct sockaddr_in cliaddr;
-  struct sockaddr_in servaddr;
 
+	//Initialization
+  int sockfd, n;
+  struct sockaddr_in servaddr;
   socklen_t len;
   char buffer[1024];
+
+	//used struct timeval because of the types of parameters required in setsockopt() function. 
+	//in other words, if we don't use struct timeval, then we cannot use setsockopt().
   struct timeval tv;
   tv.tv_sec = 1;
   tv.tv_usec = 0;
@@ -29,28 +32,21 @@ main() {
 
 
   memset(&servaddr, 0, sizeof(servaddr));
-	//memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	//Create socket on client side.
   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
 
-
-
-
-   //fill server information here too.
+   //Fill server information here too.
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(PORT);
   servaddr.sin_addr.s_addr = INADDR_ANY;
 
-      len = sizeof(servaddr);
+	//set value of len, otherwise uninitialized
+  len = sizeof(servaddr);
 
 
-  //std::cout<<"Server address client side:" << servaddr.sin_family << servaddr.sin_port << servaddr.sin_addr.s_addr << std::endl;
-  /*if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) <0) {
-    perror("BIND FAILED");
-  }*/
-
-
-
+	//check and see if connection even works. encapsulated in if statement to account for potential error.
   if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
         printf("\n Error : Connect Failed \n");
@@ -59,31 +55,32 @@ main() {
       std::cout<<"Connected successfully."<<std::endl;
     }
 
+		//If setting the timeout fails, then error. Not necessary, but is a safeguard.
+		//encapsulated in if statement to account for potential error.
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
         perror("Error");
         std::cout<<"Timed out." << std::endl;
     }
-//  std::cout<<sockfd <<std::endl;
-  for (int i = 0; i < 10; i++) {
-    //  std::cout<<&servaddr<<std::endl;
-    std::cout<<"\nPlease enter msg to be sent\n"<<std::endl;
-    fgets(buffer, 1024, stdin);
 
-    /*sendto(sockfd, (char* )buffer, sizeof buffer,
-          MSG_CONFIRM, (const struct sockaddr *)&servaddr, len);*/
+	//Loop 10 times for 10 pings. 
+  for (int i = 0; i < 10; i++) {
+
+   //send empty payload. encapsulated in if statement to account for potential error.
     if (sendto(sockfd, (char* )buffer, sizeof buffer,
           MSG_CONFIRM, (const struct sockaddr *)&servaddr, len) <0 ) {
             perror("ERROR!");
             exit(0);
           }
 
-
+		//listening
     n = recvfrom(sockfd, (char *)buffer, sizeof(buffer),
           MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
-      //    buffer[n]='\0';
+          buffer[n]='\0';
           std::cout<<n<<std::endl;
+
+					//n will be -1 if timeout or no response. Therefore check to see if n=-1. 
           if (n==-1) {
-            std::cout<<"failed to receive response." <<std::endl;
+            std::cout<<"failed to receive response." << std::endl;
             continue;
           }
     std::cout<<"Server:"<< buffer <<  std::endl;
